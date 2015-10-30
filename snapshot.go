35b41d9d94196b979320213936b219a75ff3678e
Johannes 'fish' Ziemke <fish@freigeist.org>
2015-10-30 22:37:32 +0100

    Add missing snapshot.go
    Was missed in last commit.

---
+++ snapshot.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"encoding/json"
+	"io"
+	"log"
+	"strings"
+
+	"github.com/prometheus/client_golang/prometheus"
+)
+
+type snapshotMetrics map[string]float64
+
+func snapshotCollect(ch chan<- prometheus.Metric, r io.Reader) {
+	var metrics snapshotMetrics
+	if err := json.NewDecoder(r).Decode(&metrics); err != nil {
+		log.Print(err)
+		return
+	}
+
+	for name, value := range metrics {
+		mn := strings.Join(append([]string{"mesos_master"}, strings.Split(name, "/")...), "_")
+		desc := prometheus.NewDesc(mn, "Exposed from /metrics/snapshot", nil, nil)
+		ch <- prometheus.MustNewConstMetric(desc, prometheus.UntypedValue, value)
+	}
+}
